467b8fb9e3569c0f5c1a5736b3b4266e137875c4
전산김동욱 <pg2@linkplanner.co.kr>
2020-05-06 17:36:49 +0900

    빌드 환경 작업중...

---
+++ config/webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path')
+
+process.env.NODE_ENV = 'production'
+
+module.exports = function (env, options) {
+  console.log('env', env)
+  const isEnvDevelopment = env === 'development'
+  const isEnvProduction = env === 'production'
+  return {
+    mode: isEnvProduction ? 'production' : isEnvDevelopment ? 'development' : 'none',
+    entry: './src/index.ts',
+    module: {
+      rules: [
+        {
+          test: /\.tsx?$/,
+          use: 'ts-loader',
+          exclude: /node_modules/
+        }
+      ]
+    },
+    devtool: 'source-map',
+    target: 'node',
+    resolve: {
+      extensions: ['.ts', '.js']
+    },
+    output: {
+      filename: 'bundle.js',
+      path: path.resolve(__dirname, '../dist')
+    }
+  }
+}
