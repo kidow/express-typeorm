8c4ae6142014976d8f5f973dc6962bb1d2a6f69d
전산김동욱 <pg2@linkplanner.co.kr>
2020-05-06 16:13:03 +0900

    폴더 정리

---
+++ ormconfig.js
@@ -0,0 +1,18 @@
+module.exports = {
+  type: "mysql",
+  host: "localhost",
+  port: 3306,
+  username: "root",
+  password: "1234",
+  database: "skyblue",
+  synchronize: true,
+  logging: false,
+  entities: ["src/entities/**/*.ts"],
+  migrations: ["src/migrations/**/*.ts"],
+  subscribers: ["src/subscribers/**/*.ts"],
+  cli: {
+    entitiesDir: "src/entities",
+    migrationsDir: "src/migrations",
+    subscribersDir: "src/subscriber",
+  },
+};
