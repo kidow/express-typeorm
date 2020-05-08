055abd85e3cc3c6ab86b133db553faf3190135b6
전산김동욱 <pg2@linkplanner.co.kr>
2020-05-08 14:34:47 +0900

    connection error catch

--- src/index.ts
+++ src/index.ts
@@ -18,65 +18,67 @@ import UserProfile from 'entities/UserProfile'
 
 const PORT = process.env.PORT || 4000
 
-getConnectionOptions().then((connectionOptions) =>
-  createConnection({ ...connectionOptions, namingStrategy: new SnakeNamingStrategy() })
-    .then(async ({ manager }) => {
-      // create express app
-      const app = express()
-      app.use(morgan('dev'))
-      app.use(express.json())
-      app.use(express.urlencoded({ extended: false }))
-      app.use(cors())
-      app.use(cookieParser())
+getConnectionOptions()
+  .then((connectionOptions) =>
+    createConnection({ ...connectionOptions, namingStrategy: new SnakeNamingStrategy() })
+      .then(async ({ manager }) => {
+        // create express app
+        const app = express()
+        app.use(morgan('dev'))
+        app.use(express.json())
+        app.use(express.urlencoded({ extended: false }))
+        app.use(cors())
+        app.use(cookieParser())
 
-      // register express routes from defined application routes
-      Routes.forEach((route) => {
-        app[route.method](route.route, (req: Request, res: Response, next: Function) => {
-          const result = new route.controller()[route.action](req, res, next)
-          if (result instanceof Promise) {
-            result.then((result) =>
-              result !== null && result !== undefined ? res.send(result) : undefined
-            )
-          } else if (result !== null && result !== undefined) {
-            res.json(result)
-          }
+        // register express routes from defined application routes
+        Routes.forEach((route) => {
+          app[route.method](route.route, (req: Request, res: Response, next: Function) => {
+            const result = new route.controller()[route.action](req, res, next)
+            if (result instanceof Promise) {
+              result.then((result) =>
+                result !== null && result !== undefined ? res.send(result) : undefined
+              )
+            } else if (result !== null && result !== undefined) {
+              res.json(result)
+            }
+          })
         })
+
+        // setup express app here
+        // ...
+
+        // start express server
+        app.listen(PORT)
+
+        // insert new users for test
+        const profile = await UserProfile.create({
+          age: faker.random.number(99),
+          imageUrl: faker.random.image()
+        }).save()
+        const groups: Group[] = [
+          await Group.create({ name: faker.company.companySuffix() }).save(),
+          await Group.create({ name: faker.company.companySuffix() }).save()
+        ]
+        const user = await User.create({
+          name: faker.name.lastName() + faker.name.firstName(),
+          email: `${faker.random.number(100000)}@example.com`,
+          uuid: uuid(),
+          groups,
+          profile
+        }).save()
+        const post = await Post.create({
+          title: faker.random.word(),
+          content: faker.random.words(),
+          user
+        }).save()
+        await Comment.create({
+          content: faker.lorem.sentence(),
+          user,
+          post
+        }).save()
+
+        console.log(`Express server has started on port ${PORT}.`)
       })
-
-      // setup express app here
-      // ...
-
-      // start express server
-      app.listen(PORT)
-
-      // insert new users for test
-      const profile = await UserProfile.create({
-        age: faker.random.number(99),
-        imageUrl: faker.random.image()
-      }).save()
-      const groups: Group[] = [
-        await Group.create({ name: faker.company.companySuffix() }).save(),
-        await Group.create({ name: faker.company.companySuffix() }).save()
-      ]
-      const user = await User.create({
-        name: faker.name.lastName() + faker.name.firstName(),
-        email: `${faker.random.number(100000)}@example.com`,
-        uuid: uuid(),
-        groups,
-        profile
-      }).save()
-      const post = await Post.create({
-        title: faker.random.word(),
-        content: faker.random.words(),
-        user
-      }).save()
-      await Comment.create({
-        content: faker.lorem.sentence(),
-        user,
-        post
-      }).save()
-
-      console.log(`Express server has started on port ${PORT}.`)
-    })
-    .catch((err) => console.log(err))
-)
+      .catch((err) => console.log('createConnection err: ', err))
+  )
+  .catch((err) => console.log('getConnectionOptions err: ', err))
